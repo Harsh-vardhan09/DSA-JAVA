a7ef3df92920940d31d0d8452145ade57310e443
Harsh Vardhan <harsh.vardhanp0901@gmail.com>
2026-03-16 19:22:32 +0530

    Bitwise_operator

---
+++ Math-operators/src/main/java/org/JAVA/Is_odd_even.java
@@ -0,0 +1,13 @@
+package org.JAVA;
+
+
+public class Is_odd_even {
+    public static void main(String[] args) {
+        int n=68;
+        System.out.println(isOdd(n));
+
+    }
+    private static boolean isOdd(int n){
+        return (n&1)==1;
+    }
+}
