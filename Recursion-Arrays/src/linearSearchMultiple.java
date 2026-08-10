f918b64db5db28b17a84ad8d7b76660229674e6b
Harsh <harsh.vardhanp0901@gmail.com>
2026-08-11 02:29:02 +0530

    recursion:arrays

--- Recursion-Arrays/src/linearSearchMultiple.java
+++ Recursion-Arrays/src/linearSearchMultiple.java
@@ -12,6 +12,21 @@ public class linearSearchMultiple {
        findAllIndex(arr,tar,idx+1);
    }
 
+    public ArrayList<Integer> findAllIndex2(int[] arr, int tar,int idx){
+        ArrayList<Integer> list=new ArrayList<>();
+        if(idx==arr.length){
+            return;
+        }
+
+//        this will contain answer for that function call only
+        if(arr[idx]==tar){
+            list.add(idx);
+        }
+        ArrayList<Integer> ansFromBelowCalls =findAllIndex2(arr,tar,idx+1);
+
+        list.addAll(ansFromBelowCalls);
+        return list;
+    }
     public static void main(String[] args) {
 
     }
